2d63a52de335d28089a6ad25bc86d58423455afd
neilha <neilharrold@gmail.com>
2024-06-27 14:33:47 +0100

    adds users table

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  include PublishingPlatform::SSO::User
+  serialize :permissions, type: Array, coder: YAML
+
+  def set_app_name!
+    if app_name.blank? && email.present?
+      self.app_name = email.split("@")[0]
+      save!
+    end
+  end
+end
