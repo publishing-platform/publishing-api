1368fbdde85d9b55506003203f744f746c057742
neilha <neilharrold@gmail.com>
2024-06-27 15:18:06 +0100

    adds authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::API
   include PublishingPlatform::SSO::ControllerMethods
+
+  before_action :authenticate_user!
+
+  Warden::Manager.after_authentication do |user, _, _|
+    user.set_app_name!
+  end  
 end
