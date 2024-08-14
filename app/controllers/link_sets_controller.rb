219c51446e8a6c7a350fc8285cbb424f73a6a7dc
neilha <neilharrold@gmail.com>
2024-08-14 20:18:55 +0100

    implements routes to retrieve expanded links for a given content_id

--- app/controllers/link_sets_controller.rb
+++ app/controllers/link_sets_controller.rb
@@ -8,7 +8,27 @@ class LinkSetsController < ApplicationController
     render status: response.code, json: response
   end
 
+  def expanded_links
+    json = Queries::GetExpandedLinks.call(
+      content_id,
+      with_drafts: with_drafts?,
+      generate: generate?,
+    )
+
+    render json:
+  end  
+
 private
+
+  def with_drafts?
+    # Cast the `with_drafts` query param to a real boolean, and default to
+    # `true` to preserve existing behaviour
+    ActiveModel::Type::Boolean.new.cast(params.fetch(:with_drafts, true))
+  end
+
+  def generate?
+    ActiveModel::Type::Boolean.new.cast(params.fetch(:generate, false))
+  end  
 
   def links_params
     payload.merge(content_id:)
