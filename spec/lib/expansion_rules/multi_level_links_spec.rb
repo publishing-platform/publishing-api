18f0525d0f4973d45fbfced0b32233d1514e5623
neilha <neilharrold@gmail.com>
2025-01-24 16:57:30 +0000

    completes lib specs

---
+++ spec/lib/expansion_rules/multi_level_links_spec.rb
@@ -0,0 +1,146 @@
+require "rails_helper"
+
+RSpec.describe ExpansionRules::MultiLevelLinks do
+  let(:multi_level_link_paths) do
+    [
+      %i[red blue],
+      [[:yellow]],
+      [:yellow, [:green]],
+    ]
+  end
+  let(:backwards) { false }
+
+  describe "#allowed_link_types" do
+    subject(:instance) do
+      described_class.new(multi_level_link_paths, backwards:)
+    end
+
+    context "when in forwards mode" do
+      specify do
+        expect(instance.allowed_link_types([:red])).to match([:blue])
+      end
+      specify do
+        expect(instance.allowed_link_types([:blue])).to be_empty
+      end
+      specify do
+        expect(instance.allowed_link_types([:yellow])).to match(%i[yellow green])
+      end
+      specify do
+        expect(instance.allowed_link_types(%i[yellow green])).to match([:green])
+      end
+    end
+
+    context "when in backwards mode" do
+      let(:backwards) { true }
+      specify do
+        expect(instance.allowed_link_types([:blue])).to match([:red])
+      end
+      specify do
+        expect(instance.allowed_link_types([:red])).to be_empty
+      end
+      specify do
+        expect(instance.allowed_link_types([:yellow])).to match([:yellow])
+      end
+      specify do
+        expect(instance.allowed_link_types([:green])).to match(%i[yellow green])
+      end
+    end
+  end
+
+  describe "#paths" do
+    subject do
+      described_class.new(multi_level_link_paths, backwards:)
+        .paths(length:)
+    end
+
+    context "when in forwards mode" do
+      context "when length is 1" do
+        let(:length) { 1 }
+        let(:length_1) do
+          [
+            %i[red blue],
+            [:yellow],
+            %i[yellow green],
+          ]
+        end
+        it { is_expected.to match(length_1) }
+      end
+
+      context "when paths is 3" do
+        let(:length) { 3 }
+        let(:length_3) do
+          [
+            %i[red blue],
+            %i[yellow yellow yellow],
+            %i[yellow green green],
+          ]
+        end
+        it { is_expected.to match(length_3) }
+      end
+    end
+
+    context "when in backwards mode" do
+      let(:backwards) { true }
+
+      context "when length is 2" do
+        let(:length) { 2 }
+        let(:length_2) do
+          [
+            %i[blue red],
+            %i[yellow yellow],
+            %i[green yellow],
+          ]
+        end
+        it { is_expected.to match(length_2) }
+      end
+
+      context "when paths is 4" do
+        let(:length) { 4 }
+        let(:length_4) do
+          [
+            %i[blue red],
+            %i[yellow yellow yellow yellow],
+            %i[green green green yellow],
+          ]
+        end
+        it { is_expected.to match(length_4) }
+      end
+    end
+  end
+
+  describe ".next_allowed_link_types" do
+    subject do
+      described_class.new(multi_level_link_paths, backwards:)
+        .next_allowed_link_types(link_types, link_types_path)
+    end
+
+    context "when there is an empty link_types_path" do
+      let(:link_types) { %i[yellow red] }
+      let(:link_types_path) { [] }
+
+      it { is_expected.to match(yellow: %i[yellow green], red: [:blue]) }
+    end
+
+    context "when there is a link_types_path" do
+      let(:link_types) { [:green] }
+      let(:link_types_path) { [:yellow] }
+
+      it { is_expected.to match(green: [:green]) }
+    end
+
+    context "when there are no links" do
+      let(:link_types) { [:red] }
+      let(:link_types_path) { [:green] }
+
+      it { is_expected.to be_empty }
+    end
+
+    context "when the class is initialized with backwards true" do
+      let(:backwards) { true }
+      let(:link_types) { [:blue] }
+      let(:link_types_path) { [] }
+
+      it { is_expected.to match(blue: [:red]) }
+    end
+  end
+end
---
+++ spec/lib/link_expansion/edition_diff_spec.rb
@@ -0,0 +1,148 @@
+require "rails_helper"
+
+RSpec.describe LinkExpansion::EditionDiff do
+  let(:content_id) { SecureRandom.uuid }
+  let(:document) { create(:document, content_id:) }
+
+  let!(:previous_edition) do
+    create(
+      :superseded_edition,
+      document:,
+      title: "Foo",
+      base_path: "/foo",
+    )
+  end
+
+  let!(:current_edition) do
+    create(
+      :live_edition,
+      document:,
+      title: "Bar",
+      base_path: "/foo",
+      user_facing_version: 2,
+    )
+  end
+
+  let(:new_draft_edition) do
+    create(
+      :draft_edition,
+      document:,
+      title: "Bar",
+      base_path: "/foo",
+      user_facing_version: 3,
+      public_updated_at: current_edition.public_updated_at,
+      first_published_at: current_edition.first_published_at,
+    )
+  end
+
+  subject { described_class.new(current_edition) }
+
+  shared_examples "should have changes" do
+    it "should have changes" do
+      expect(subject.present?).to eq(true)
+    end
+  end
+
+  shared_examples "shouldn't have changes" do
+    it "shouldn't have changes" do
+      expect(subject.present?).to eq(false)
+    end
+  end
+
+  context "diff in title" do
+    include_examples "should have changes"
+
+    it "should have the correct changed fields" do
+      expect(subject.fields).to eq(%i[title])
+    end
+  end
+
+  context "diff in title, document_type and base_path" do
+    let!(:current_edition) do
+      create(
+        :live_edition,
+        document:,
+        title: "Bar",
+        base_path: "/bar",
+        user_facing_version: 2,
+        document_type: "new_type",
+      )
+    end
+
+    include_examples "should have changes"
+
+    it "should have the correct changed fields" do
+      expect(subject.fields).to match_array(%i[api_path base_path document_type title])
+    end
+  end
+
+  context "diff inside details hash" do
+    let!(:previous_edition) do
+      create(
+        :superseded_edition,
+        document:,
+        document_type: "organisation",
+        base_path: "/foo",
+        details: { abbreviation: "foo" },
+      )
+    end
+
+    context "with a field that matters" do
+      let!(:current_edition) do
+        create(
+          :live_edition,
+          document:,
+          user_facing_version: 2,
+          document_type: "organisation",
+          base_path: "/foo",
+          details: { abbreviation: "bar" },
+        )
+      end
+
+      include_examples "should have changes"
+
+      it "should have the correct changed fields" do
+        expect(subject.fields).to eq(%i[details])
+      end
+    end
+
+    context "with a field that doesn't matter" do
+      let!(:current_edition) do
+        create(
+          :live_edition,
+          document:,
+          user_facing_version: 2,
+          document_type: "organisation",
+          base_path: "/foo",
+          details: { abbreviation: "foo", unrelated_field: "bar" },
+        )
+      end
+
+      include_examples "shouldn't have changes"
+    end
+  end
+
+  context "multiple versions" do
+    before { current_edition.supersede }
+    subject { described_class.new(new_draft_edition) }
+    include_examples "shouldn't have changes"
+  end
+
+  context "no previous item" do
+    let!(:previous_edition) { nil }
+    include_examples "should have changes"
+  end
+
+  context "provide the edition to compare" do
+    context "given an empty hash" do
+      subject { described_class.new(new_draft_edition, previous_edition: {}) }
+      include_examples "should have changes"
+    end
+
+    context "given an edition" do
+      let(:presented_item) { new_draft_edition.to_h.deep_stringify_keys }
+      subject { described_class.new(new_draft_edition, previous_edition: presented_item) }
+      include_examples "shouldn't have changes"
+    end
+  end
+end
