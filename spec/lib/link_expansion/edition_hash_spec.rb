94fd9fd7007eb90e6f9be5a6301c900a7187d58a
neilha <neilharrold@gmail.com>
2025-02-19 08:38:54 +0000

    adds edition hash spec

---
+++ spec/lib/link_expansion/edition_hash_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe LinkExpansion::EditionHash do
+  describe "from" do
+    it "accepts a nil argument" do
+      expect(described_class.from(nil)).to be_nil
+    end
+    it "accepts an array argument" do
+      expect(described_class.from(%w[123])).to include(
+        ExpansionRules::POSSIBLE_FIELDS_FOR_LINK_EXPANSION[0] => "123",
+      )
+    end
+    it "accepts a Hash argument" do
+      expect(described_class.from(base_path: "/test")).to include(
+        base_path: "/test",
+      )
+    end
+    it "accepts an Edition argument" do
+      edition = build(:live_edition)
+      edition_hash = described_class.from(edition)
+      expect(edition_hash[:content_id]).to eq(edition.content_id)
+    end
+    it "raises an ArgumentError otherwise" do
+      expect {
+        described_class.from(Time.zone.now)
+      }.to raise_error ArgumentError
+    end
+  end
+end
