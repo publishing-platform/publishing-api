034df65a424428e9608cfbc81424a50c4805f3ab
neilha <neilharrold@gmail.com>
2025-02-19 12:35:35 +0000

    adds base_command specs

---
+++ spec/commands/base_command_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+RSpec.describe Commands::BaseCommand do
+  let(:top_level_worker) { double(:top_level_worker, some_method: nil) }
+  let(:nested_worker) { double(:nested_worker, some_method: nil) }
+
+  let(:top_level_command) do
+    Class.new(Commands::BaseCommand) do
+      def self.name
+        "Commands::TopLevelCommand"
+      end
+
+      def call
+        after_transaction_commit do
+          payload[:top_level_worker].some_method
+        end
+
+        Commands::NestedCommand.call(payload, callbacks:, nested: true)
+      end
+    end
+  end
+
+  let(:nested_command) do
+    Class.new(Commands::BaseCommand) do
+      def self.name
+        "Commands::NestedCommand"
+      end
+
+      def call
+        after_transaction_commit do
+          payload[:nested_worker].some_method
+        end
+      end
+    end
+  end
+
+  before { stub_const("Commands::NestedCommand", nested_command) }
+
+  describe "callbacks for nested commands" do
+    it "executes callbacks at the top level of the command tree" do
+      expect(top_level_command).to receive(:execute_callbacks)
+      expect(nested_command).not_to receive(:execute_callbacks)
+
+      top_level_command.call({})
+    end
+
+    it "executes all callbacks from every level of the command tree" do
+      expect(top_level_worker).to receive(:some_method)
+      expect(nested_worker).to receive(:some_method)
+
+      payload = {
+        top_level_worker:,
+        nested_worker:,
+      }
+
+      top_level_command.call(payload)
+    end
+  end
+end
