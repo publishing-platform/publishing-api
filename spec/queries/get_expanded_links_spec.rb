f20ade69ec215aa00fee31db574d4d1b53260066
neilha <neilharrold@gmail.com>
2025-02-20 08:13:17 +0000

    adds get_expanded_links query spec

---
+++ spec/queries/get_expanded_links_spec.rb
@@ -0,0 +1,92 @@
+require "rails_helper"
+
+RSpec.describe Queries::GetExpandedLinks do
+  let(:content_id) { SecureRandom.uuid }
+  let(:with_drafts) { true }
+  let(:generate) { false }
+
+  subject(:result) do
+    described_class.call(
+      content_id,
+      with_drafts:,
+      generate:,
+    )
+  end
+
+  context "when there isn't a document or link set associated with the content id" do
+    it "raises a command error" do
+      expect { result }.to raise_error(
+        CommandError,
+        /could not find links for content_id: #{content_id}/i,
+      )
+    end
+  end
+
+  context "when generate is false" do
+    context "and there are expanded links stored" do
+      let(:updated_at) { Time.zone.parse("2017-07-27 16:01:01").utc }
+      let(:expanded_links) do
+        {
+          link_type: { content_id: SecureRandom.uuid },
+        }
+      end
+
+      before do
+        create(
+          :expanded_links,
+          content_id:,
+          with_drafts:,
+          expanded_links:,
+          updated_at:,
+        )
+      end
+
+      it "returns the data from expanded links" do
+        expect(result).to match(
+          generated: updated_at.iso8601,
+          expanded_links: expanded_links.as_json,
+        )
+      end
+    end
+
+    context "but there are not expanded links stored" do
+      let(:link_set_lock_version) { 3 }
+
+      before do
+        create(
+          :link_set,
+          document: create(:document, content_id:),
+          content_id:,
+          links_hash: {},
+          stale_lock_version: link_set_lock_version,
+        )
+      end
+
+      it "generates the links" do
+        Timecop.freeze do
+          expect(result).to match(
+            a_hash_including(
+              generated: Time.zone.now.utc.iso8601,
+              expanded_links: {},
+            ),
+          )
+        end
+      end
+
+      it "returns the lock version" do
+        expect(result).to match(a_hash_including(version: link_set_lock_version))
+      end
+    end
+  end
+
+  context "when generate is true" do
+    let(:generate) { false }
+    context "and there is not a link set associated with the content id" do
+      before { create(:document, content_id:) }
+
+      it "returns a version of 0" do
+        expect(result).to match(a_hash_including(version: 0))
+      end
+    end
+  end
+end
