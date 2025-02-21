de20d8163b7c3d8512484faaa246507a8292a920
neilha <neilharrold@gmail.com>
2025-02-21 14:33:02 +0000

    expanded_links request specs

---
+++ spec/requests/expanded_links_request_spec.rb
@@ -0,0 +1,94 @@
+require "rails_helper"
+
+RSpec.describe "/expanded-links", type: :request do
+  let(:content_id) { SecureRandom.uuid }
+  let(:updated_at) { Time.zone.parse("2017-07-27 16:44:00") }
+
+  describe "GET /expanded_links" do
+    context "when requesting a content_id that isn't known" do
+      it "returns 404" do
+        get "/expanded-links/#{content_id}"
+
+        expect(parsed_response).to eql(
+          "error" => {
+            "code" => 404,
+            "message" => "Could not find links for content_id: #{content_id}",
+          },
+        )
+      end
+    end
+
+    context "when request specifies to exclude drafts" do
+      let(:expanded_links) do
+        {
+          non_draft_organisations: [{ content_id: SecureRandom.uuid }],
+          available_translations: [{ content_id: }],
+        }
+      end
+
+      before do
+        create(
+          :expanded_links,
+          content_id:,
+          with_drafts: false,
+          expanded_links:,
+          updated_at:,
+        )
+      end
+
+      it "it returns the links " do
+        get "/expanded-links/#{content_id}", params: { with_drafts: false }
+
+        puts parsed_response
+        expect(parsed_response).to eql(
+          "generated" => updated_at.utc.iso8601,
+          "expanded_links" => expanded_links.as_json,
+        )
+      end
+    end
+
+    context "when specifying to generate the links" do
+      let(:linked_content_id) { SecureRandom.uuid }
+
+      let!(:edition) do
+        create(
+          :live_edition,
+          document: create(:document, content_id:),
+          base_path: "/some-path",
+          links_hash: { organisations: [linked_content_id] },
+        )
+      end
+
+      let!(:linked_edition) do
+        create(
+          :live_edition,
+          document: create(:document, content_id: linked_content_id),
+          base_path: "/another-path",
+        )
+      end
+
+      let(:expanded_links) do
+        {
+          "organisations" => [
+            a_hash_including(
+              "content_id" => linked_content_id,
+              "base_path" => "/another-path",
+            ),
+          ],
+        }
+      end
+
+      it "generates the links at runtime" do
+        Timecop.freeze do
+          get "/expanded-links/#{content_id}", params: { generate: true }
+
+          expect(parsed_response).to match(
+            "generated" => Time.zone.now.utc.iso8601,
+            "expanded_links" => expanded_links,
+            "version" => 0,
+          )
+        end
+      end
+    end
+  end
+end
