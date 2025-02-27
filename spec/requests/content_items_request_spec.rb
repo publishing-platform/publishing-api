6178ec23c3cafce1b5b5e1c584ba6429f051caf3
neilha <neilharrold@gmail.com>
2025-02-27 21:42:37 +0000

    continues work on content_item request specs

--- spec/requests/content_items_request_spec.rb
+++ spec/requests/content_items_request_spec.rb
@@ -2,7 +2,14 @@ require "rails_helper"
 
 RSpec.describe "/content", type: :request do
   let(:content_id) { SecureRandom.uuid }
-  let(:request_path) { "/links/#{content_id}" }
+  let(:validator) do
+    instance_double(SchemaValidator, valid?: true, errors: [])
+  end
+
+  before do
+    allow(SchemaValidator).to receive(:new).and_return(validator)
+    stub_request(:any, /content-store/)
+  end
 
   describe "GET /index" do
     let(:previous_edition) do
@@ -227,6 +234,236 @@ RSpec.describe "/content", type: :request do
             { "base_path" => "/bar" },
           ])
         end
+      end
+    end
+
+    context "with a publishing_app param" do
+      let!(:other_edition) do
+        create(
+          :draft_edition,
+          publishing_app: "organisations-publisher",
+        )
+      end
+
+      it "filters by publishing app" do
+        get "/content", params: { publishing_app: "organisations-publisher" }
+
+        expect(response.status).to eq(200)
+        expect(parsed_response["results"].count).to eq(1)
+        expect(parsed_response["results"].all? { |i| i["publishing_app"] == "organisations-publisher" }).to be true
+      end
+    end
+
+    context "with a state param" do
+      let!(:other_edition) { create(:draft_edition) }
+
+      it "filters by state" do
+        get "/content", params: { states: %w[superseded] }
+
+        expect(response.status).to eq(200)
+        expect(parsed_response["results"].count).to eq(1)
+        expect(parsed_response["results"][0]["content_id"]).to eq(edition.content_id)
+      end
+    end
+
+    context "with link filtering params" do
+      let(:document) { create(:document, content_id:) }
+      let!(:edition) do
+        create(
+          :live_edition,
+          base_path: "/foo",
+          document:,
+        )
+      end
+
+      before do
+        org_content_id = SecureRandom.uuid
+        link_set = create(:link_set, content_id:)
+        create(:link, link_set:, target_content_id: org_content_id)
+
+        get "/content", params: { fields: %w[content_id], link_primary_publishing_organisation: org_content_id }
+      end
+
+      it "responds with the editions for the given primary publishing organistion" do
+        expect(response.status).to eq(200)
+        expect(parsed_response["results"].count).to eq(1)
+        expect(parsed_response["results"].first.fetch("content_id")).to eq(content_id)
+      end
+    end
+  end
+
+  describe "GET /show" do
+    context "for an existing edition" do
+      let!(:document) { create(:document, content_id:) }
+      let!(:edition) { create(:edition, document:) }
+      let(:request_path) { "/content/#{content_id}" }
+
+      it "responds with the edition" do
+        get request_path
+
+        expect(response.status).to eq(200)
+        expect(parsed_response["content_id"]).to eq(content_id)
+      end
+
+      context "with edition links" do
+        let(:target_content_id) { SecureRandom.uuid }
+
+        before do
+          edition.links.create!(
+            link_type: "primary_publishing_organisation",
+            target_content_id:,
+          )
+        end
+
+        it "includes the edition links in the JSON" do
+          get request_path
+
+          expect(response.status).to eq(200)
+          expect(parsed_response["links"]["primary_publishing_organisation"]).to_not be_empty
+          expect(parsed_response["links"]["primary_publishing_organisation"]).to match_array([target_content_id])
+        end
+      end
+    end
+
+    context "for a non-existent edition" do
+      let(:request_path) { "/content/#{SecureRandom.uuid}" }
+
+      it "responds with 404" do
+        get request_path
+
+        expect(response.status).to eq(404)
+      end
+    end
+  end
+
+  describe "PUT /put_content" do
+    let(:request_path) { "/content/#{content_id}" }
+
+    context "with valid request params for a new edition" do
+      before do
+        put request_path, params: content_item_params.to_json
+      end
+
+      it "responds with 200" do
+        expect(response.status).to eq(200)
+      end
+
+      it "responds with the new edition" do
+        new_edition = Edition.with_document.find_by!("documents.content_id": content_id)
+        presented_content_item = Presenters::Queries::ContentItemPresenter.present(
+          new_edition,
+          include_warnings: true,
+        )
+
+        expect(response.body).to eq(presented_content_item.to_json)
+      end
+    end
+
+    context "with valid request params for an existing edition" do
+      let(:edition) { create(:edition) }
+      let(:content_id) { edition.document.content_id }
+
+      before do
+        put request_path, params: content_item_params.to_json
+      end
+
+      it "responds with 200" do
+        expect(response.status).to eq(200)
+      end
+
+      it "responds with the updated edition" do
+        updated_edition = Edition.with_document.find_by!("documents.content_id": content_id)
+        presented_content_item = Presenters::Queries::ContentItemPresenter.present(
+          updated_edition,
+          include_warnings: true,
+        )
+
+        expect(response.body).to eq(presented_content_item.to_json)
+      end
+    end
+
+    context "with invalid json" do
+      it "responds with 400" do
+        put request_path, params: "Not JSON"
+        expect(response.status).to eq(400)
+      end
+    end
+
+    context "when draft content store is not running but draft 502s are suppressed" do
+      before do
+        @swallow_connection_errors = PublishingApi.swallow_connection_errors
+        PublishingApi.swallow_connection_errors = true
+        stub_request(:put, %r{^http://draft-content-store.*/content/.*})
+          .to_return(status: 502)
+      end
+
+      after do
+        PublishingApi.swallow_connection_errors = @swallow_connection_errors
+      end
+
+      it "returns the normal 200 response" do
+        put request_path, params: content_item_params.to_json
+
+        parsed_response
+        expect(response.status).to eq(200)
+        expect(parsed_response["content_id"]).to eq(content_id)
+        expect(parsed_response["title"]).to eq(content_item_params[:title])
+      end
+    end
+
+    context "when draft content store times out" do
+      before do
+        stub_request(:put, PublishingPlatformLocation.find("draft-content-store") + "/content#{base_path}").to_timeout
+      end
+
+      it "returns an appropriate error" do
+        put request_path, params: content_item_params.to_json
+
+        expect(response.status).to eq(500)
+        expect(parsed_response).to eq(
+          "error" => {
+            "code" => 500,
+            "message" => "Unexpected error from the downstream application: Timed out connecting to server",
+          },
+        )
+      end
+    end
+
+    it "reserves path" do
+      put request_path, params: content_item_params.to_json
+
+      expect(PathReservation.count).to eq(1)
+      expect(PathReservation.first.base_path).to eq(content_item_params[:base_path])
+      expect(PathReservation.first.publishing_app).to eq(content_item_params[:publishing_app])
+    end
+
+    context "when a base path is occupied by a 'regular' edition" do
+      before do
+        create(
+          :draft_edition,
+          base_path:,
+        )
+      end
+
+      it "cannot be replaced by another draft 'regular' edition" do
+        put request_path, params: content_item_params.to_json
+
+        expect(response.status).to eq(422)
+      end
+    end
+
+    context "when a base path is occupied by a published 'regular' edition" do
+      before do
+        create(
+          :live_edition,
+          base_path:,
+        )
+      end
+
+      it "can be replaced by another 'regular' edition" do
+        put request_path, params: content_item_params.to_json
+
+        expect(response.status).to eq(200)
       end
     end
   end
