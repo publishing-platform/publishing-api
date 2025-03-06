2106c08002495d2804fe0d16d18bbb87988dcc03
neilha <neilharrold@gmail.com>
2025-03-06 18:35:00 +0000

    more specs

--- spec/commands/put_content_spec.rb
+++ spec/commands/put_content_spec.rb
@@ -241,5 +241,37 @@ RSpec.describe Commands::PutContent do
         end
       end
     end
+
+    describe "race conditions" do
+      let(:document) do
+        create(
+          :document,
+          content_id:,
+          stale_lock_version: 5,
+        )
+      end
+
+      let!(:edition) do
+        create(
+          :live_edition,
+          document:,
+          user_facing_version: 5,
+          first_published_at: 1.year.ago,
+          base_path:,
+        )
+      end
+
+      it "copes with race conditions" do
+        described_class.call(payload)
+        Commands::Publish.call({ content_id: })
+
+        thread1 = Thread.new { described_class.call(payload) }
+        thread2 = Thread.new { described_class.call(payload) }
+        thread1.join
+        thread2.join
+
+        expect(Edition.all.pluck(:state)).to match_array(%w[superseded published draft])
+      end
+    end
   end
 end
